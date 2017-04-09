9b061657123701d175cc30bcef75d479d339f7f2
Q. Zhang <aznyqjai@gmail.com>
2017-04-09 00:51:54 -0400

    Add SearchForm component

--- app/assets/javascripts/components/event_application.js.jsx
+++ app/assets/javascripts/components/event_application.js.jsx
@@ -2,9 +2,11 @@ var EventApplication = React.createClass({
   getInitialState: function() {
     return { events: [] };
   },
+
   componentDidMount: function() {
     this.getDataFromApi();
   },
+
   getDataFromApi: function() {
     var self = this;
     $.ajax({
@@ -17,12 +19,22 @@ var EventApplication = React.createClass({
       }
     });
   },
+
+  handleSearch: function(events) {
+    this.setState({ events: events });
+  },
+
   render: function() {
     return(
       <div className="container">
         <div className="jumbotron">
           <h1>ReactJS Tutorial</h1>
           <p>by Piotr Jaworski</p>
+        </div>
+        <div className="row">
+          <div className="col-md-4">
+            <SearchForm handleSearch={this.handleSearch} />
+          </div>
         </div>
         <div className="row">
           <div className="col-md-12">
