e9318fcba2c32bc719925adfc901ce4a950fac30
Q. Zhang <aznyqjai@gmail.com>
2017-04-08 20:00:01 -0400

    routes correct

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  resources :events
-  get 'dashboard/index'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'dashboard#index'
+  namespace :api do
+    resources :events
+  end
+
 end
+
+
